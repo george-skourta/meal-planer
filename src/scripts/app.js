121c9e7ea8296ef81746ad28d69889366ea8984a
george-skourta <shkurtageorge@gmail.com>
2021-02-10 13:37:26 +0200

    Done with layout.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,7 @@
 // Change Section.
-function changeSection() {
-    var link = event.target;
-    var section = '.' + link.dataset.section
-    var links = document.querySelectorAll('.sidebar__links-item');
-
-    links.forEach(function(el) {
-        el.classList.remove('sidebar__links-item--active')
-    });
-
-    link.classList.add('sidebar__links-item--active');
-
-    document.querySelectorAll('.section').forEach(function(el) {
-        el.style.display = 'none';
-    })
-
-    document.querySelector(section).style.display = 'block';
+function generateList() {
+    var items = [];
+    items = document.querySelectorAll('.meal-item');
+    
+    alert(items);
 }
