cdf11532d9273bf7bd18065b35b5e84ab75c1d22
george-skourta <shkurtageorge@gmail.com>
2021-02-10 16:15:49 +0200

    Removed everything and started from the begging.

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
+  
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: "hidden",
     webPreferences: {
+      
       nodeIntegration: true
+
     }
   })
+  
+  win.maximize();
 
   win.loadFile('index.html')
 }
--- src/scripts/app.js
+++
@@ -1,7 +0,0 @@
-// Change Section.
-function generateList() {
-    var items = [];
-    items = document.querySelectorAll('.meal-item');
-    
-    alert(items);
-}
